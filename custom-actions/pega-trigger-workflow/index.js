c77b3de3744233c96aeb985ce57d681dc74383a9
Sud <sudarsana.ala@gmail.com>
2022-08-04 19:39:27 +0530

    updated the code

--- custom-actions/pega-trigger-workflow/index.js
+++ custom-actions/pega-trigger-workflow/index.js
@@ -2,6 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 const qs = require('qs');
+const {formatJson, logErrors} = require('./utils')
+
 
 //pipeline-mapping.json should come from the calling workflow repo
 const pipelineMapping = require('./pipeline-mapping.json');
@@ -15,10 +17,10 @@ const PEGA_DM_CLIENT_SECRET = core.getInput('PEGA_DM_CLIENT_SECRET');
 const PEGA_TARGET_APP = core.getInput('PEGA_TARGET_APP');
 const PEGA_PROD_NAME = core.getInput('PEGA_PROD_NAME');
 const PEGA_PROD_VERSION = core.getInput('PEGA_PROD_VERSION');
-const PEGA_DEPLOYMENT_WAIT_TIME = core.getInput('PEGA_DEPLOYMENT_WAIT_TIME') || 10 * 60; //default 10 MINUTES   
-const IDLE_TIME_INTERVAL = core.getInput("IDLE_TIME_INTERVAL") || 1 * 60; // deafult 1 minute
+const PEGA_DEPLOYMENT_WAIT_TIME = core.getInput('PEGA_DEPLOYMENT_WAIT_TIME') || 10; //default 10 MINUTES   
+const IDLE_TIME_INTERVAL = core.getInput("IDLE_TIME_INTERVAL") || 1; // deafult 1 minute
 //PEGA PIPELINE ID
-const PEGA_PIEPLINE_ID = pipelineMapping["PEGA_TARGET_APP"];
+const PEGA_PIEPLINE_ID = pipelineMapping["PEGA_TARGET_APP"] || "Pipeline-QEKH0";
 
 //Log all details to console
 console.log(`PEGA_DM_REST_URL: ${PEGA_DM_REST_URL}`);
@@ -46,11 +48,8 @@ const DEPLOYMENT_STATUSES = [
   "Resolved-Aborted"
 ];
 
-
-
-
 //Global Variables
-let access_token = ""
+let access_token = "";
 
 //main function (entry point)
 async function runAction() {
@@ -59,15 +58,16 @@ async function runAction() {
 
     await getAccessToken();
     await updatePipeline();
-    let deploymentID = await triggerPipeline();
+    let deploymentID = await triggerPipeline();    
     await waitForDeploymentToComplete(deploymentID);
-
   } catch (error) {
+    console.log(error.message)
     core.setFailed(error.message);
   }
 }
 
 async function getAccessToken() {
+  console.log("Getting Access Token");
   let data = qs.stringify({
     'client_id': PEGA_DM_CLIENT_ID,
     'client_secret': PEGA_DM_CLIENT_SECRET,
@@ -89,7 +89,9 @@ async function getAccessToken() {
   return access_token;
 }
 
-async function getPielineData() {
+async function getPipelineData() {
+  let response = {};
+  console.log(`Getting Pipeline Data for ${PEGA_PIEPLINE_ID}`)
   let config = {
     method: 'get',
     url: `${PEGA_DM_REST_URL}/DeploymentManager/v1/pipelines/${PEGA_PIEPLINE_ID}`,
@@ -98,70 +100,89 @@ async function getPielineData() {
     }
   };
 
-  let response = await axios(config);
-  console.log(`response is : ${response.data}`);
-
-  if (response.data.errors) {
-    console.log("Token has expired. Getting new token");
-    await getAccessToken();
-    console.log("Re-requesting the pipeline data");
-    config.headers.Authorization = `Bearer ${access_token}`;
+  try {
     response = await axios(config);
-    console.log(`response is : ${response.data}`);
+    console.log("response is : ", formatJson(response.data));
+  } catch (error) {
+    if (error.response.status === 401) {
+      console.log("Token has expired. Getting new token");
+      await getAccessToken();
+      console.log(`Re-requesting the pipeline data for ${PEGA_PIEPLINE_ID}`);
+      config.headers.Authorization = `Bearer ${access_token}`;
+      response = await axios(config);
+      console.log("response is : ", formatJson(response.data));
+    } else {
+      throw error;
+    }
   }
   return response.data;
 }
 
 async function updatePipeline() {
+  try {
 
-  let pipelineData = await getPielineData(access_token);
+    let pipelineData = await getPipelineData(access_token);
 
-  let existing_product_name = pipelineData.pipelineParameters.filter(item => item.name == "productName")[0];
-  let existing_product_version = pipelineData.pipelineParameters.filter(item => item.name == "productVersion")[0].value;
-  let isUpdateRequired = false;
+    //Checking if updating pipelie is required.
+    console.log(`Checking if updating pipelie is required`);
+    let existing_product_name = pipelineData.pipelineParameters.filter(item => item.name === "productName")[0];
+    let existing_product_version = pipelineData.pipelineParameters.filter(item => item.name === "productVersion")[0];
+    let isUpdateRequired = false;
 
-  if (existing_product_name.value != PEGA_PROD_NAME) {
-    isUpdateRequired = true;
-    existing_product_name.value = PEGA_PROD_NAME;
-  }
-
-  if (existing_product_version.value != PEGA_PROD_VERSION) {
-    isUpdateRequired = true;
-    existing_product_name.value = PEGA_PROD_VERSION;
-  }
-
-  if (isUpdateRequired) {
-
-    console.log("Update is required for pipeline Data, updating");
-    console.log(pipelineData);
-
-    let config = {
-      method: 'put',
-      url: `${PEGA_DM_REST_URL}/DeploymentManager/v1/pipelines/${PEGA_PIEPLINE_ID}`,
-      headers: {
-        'Authorization': `Bearer ${access_token}`
-      },
-      data: pipelineData
-    };
-
-    let response = await axios(config);
-    console.log(`response is : ${response.data}`);
-
-    if (response.data.errors) {
-      console.log("Token has expired. Getting new token");
-      let access_token = getAccessToken();
-      console.log("Re-requesting the pipeline data");
-      config.headers.Authorization = `Bearer ${access_token}`;
-      response = await axios(config);
-      console.log(`response is : ${response.data}`);
+    if (existing_product_name.value != PEGA_PROD_NAME) {
+      isUpdateRequired = true;
+      existing_product_name.value = PEGA_PROD_NAME;
     }
-    return response.data;
-  } else {
-    console.log("No Update required, skipping the step");
+
+    if (existing_product_version.value != PEGA_PROD_VERSION) {
+      isUpdateRequired = true;
+      existing_product_version.value = PEGA_PROD_VERSION;
+    }
+
+    if (isUpdateRequired) {
+
+      let response = {}
+      console.log("Update is required for pipeline Data, updating", formatJson(pipelineData));
+
+      let config = {
+        method: 'put',
+        url: `${PEGA_DM_REST_URL}/DeploymentManager/v1/pipelines/${PEGA_PIEPLINE_ID}`,
+        headers: {
+          'Authorization': `Bearer ${access_token}`
+        },
+        data: JSON.stringify(pipelineData)
+      };
+
+      try {
+        response = await axios(config);
+        console.log("response is : ", formatJson(response.data));
+
+
+      } catch (error) {
+        if (error.response.status === 401) {
+          console.log("Token has expired. Getting new token");
+          let access_token = getAccessToken();
+          console.log("Re-requesting the update pipeline");
+          config.headers.Authorization = `Bearer ${access_token}`;
+          response = await axios(config);
+          console.log("response is : ", formatJson(response.data));
+        } else {
+          throw error;
+        }
+      }
+      return response.data;
+    } else {
+      console.log("No Update required, skipping the step");
+    }
+  } catch (error) {
+    console.log("Error while updating the pipelline:", formatJson(error));
+    throw error;
   }
 }
 
 async function triggerPipeline() {
+  let response = {};
+  console.log(`Triggering Deployment for ${PEGA_PIEPLINE_ID}`);
   let config = {
     method: 'post',
     url: `${PEGA_DM_REST_URL}/DeploymentManager/v1/pipelines/${PEGA_PIEPLINE_ID}/deployments`,
@@ -169,83 +190,87 @@ async function triggerPipeline() {
       'Authorization': `Bearer ${access_token}`
     }
   };
-
-  let response = await axios(config);
-  console.log(`response is : ${response.data}`);
-
-  if (response.data.errors) {
-    console.log("Token has expired. Getting new token");
-    await getAccessToken();
-    console.log("Re-requesting the pipeline data");
-    config.headers.Authorization = `Bearer ${access_token}`;
+  try {
     response = await axios(config);
-    console.log(`response is : ${response.data}`);
-  }
-  return response.data.deploymentID;
-}
-
-async function logErrors(response){
-  let errorTasks = response.taskList.filter(x=> x.status !== "Resolved-Completed");
-  let errorMessages = []
-  let taskErrorMessages = [];
-  await errorTasks.forEach(x=> {
-    errorMessages = x.errors;
-    taskErrorMessages.push({'taskName': x.taskType, 'errors': errorMessages});
-  });
-  console.log(taskErrorMessages);
-  return taskErrorMessages;
-}
-
-async function waitForDeploymentToComplete(deploymentID) {
-
-  let response={}
-  let deploymentStatus = "";
-  let isInProgress = true;
-  let totalTime = 0; // 1 minute
-
-  do {
-    await new Promise(resolve => setTimeout(resolve, IDLE_TIME_INTERVAL * 1000)); //sleep for the specified idle time
-    totalTime += IDLE_TIME_INTERVAL;
-    let config = {
-      method: 'get',
-      url: `${PEGA_DM_REST_URL}/DeploymentManager/v1/v1/deployments/${deploymentID}`,
-      headers: {
-        'Authorization': `Bearer ${access_token}`
-      }
-    };
-
-    response = await axios(config);
-    console.log(`response is : ${response.data}`);
-
-    if (response.data.errors) {
+    console.log(`trigger deployment response is :`, formatJson(response.data));
+  } catch (error) {
+    if (error.response.status === 401) {
       console.log("Token has expired. Getting new token");
       await getAccessToken();
       console.log("Re-requesting the pipeline data");
       config.headers.Authorization = `Bearer ${access_token}`;
       response = await axios(config);
-      console.log(`response is : ${response.data}`);
+      console.log(`response is : `, formatJson(response.data));
+    } else {
+      throw error;
+    }
+  }
+  return response.data.deploymentID;
+}
+
+async function waitForDeploymentToComplete(deploymentID) {
+
+  console.log("Waiting for the deployment to complete or error out");
+
+  let response = {}
+  let deploymentStatus = "";
+  let isInProgress = true;  
+  let totalTime = 0; // 1 minute 
+  do {
+    console.log(`Sleeping for ${IDLE_TIME_INTERVAL} minutes`)
+    await new Promise(resolve => setTimeout(resolve, IDLE_TIME_INTERVAL * 60 * 1000)); //sleep for the specified idle time
+    totalTime += IDLE_TIME_INTERVAL;
+    let config = {
+      method: 'get',
+      url: `${PEGA_DM_REST_URL}/DeploymentManager/v1/deployments/${deploymentID}`,
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    };
+
+    try {
+      response = await axios(config);
+      console.log(`deployment status response is : `, formatJson(response.data));
+    } catch (error) {
+      if (error.response.status === 401) {
+        console.log("Token has expired. Getting new token");
+        await getAccessToken();
+        console.log("Re-requesting the deployment status");
+        config.headers.Authorization = `Bearer ${access_token}`;
+        response = await axios(config);
+        console.log("response is : ", formatJson(response.data));
+      } else {
+        throw error;
+      }
     }
 
     deploymentStatus = response.data.status;
     isInProgress = (deploymentStatus === "Open-Queued" || deploymentStatus == "Open-InProgress") ? true : false;
-  } while (totalTime < PEGA_DEPLOYMENT_WAIT_TIME && isInProgress);
+    let manualSteps = response.data.taskList.filter(x => x.status === "Pending-Input");
+    if (manualSteps.length > 0) {
+      let manualStep = manualSteps[0];
+      console.log(`Approval is required for manual step: "${manualStep.taskType}" in stage: "${manualStep.stageName}"`)
+      return;
+    }
+
+  } while (totalTime <= PEGA_DEPLOYMENT_WAIT_TIME  && isInProgress);
 
   switch (deploymentStatus) {
     case 'Resolved-Completed':
       console.log("Deployment Successful");
       break;
     case 'Open-Queued':
-      console.log("Deployment not started yet, check the previous builds");
+      console.log("Deployment not started yet, check the previous deployment status");
       break;
     case 'Open-InProgress':
-      console.log("Deployment is still in progress, even after 10 minutes. Check if manual approval is required.");
+      console.log("Deployment is still in progress, even after 10 minutes");
       break;
     case 'Open-Error' || 'Resolved-Rejected':
-      console.log("There is an error/rejection in the deployment, check the error and corrective action.");
-      await logErrors(response);
-      break;
+      console.log("There is an error/rejection in the deployment, check the error and take corrective action.");
+      let errrorMessages = await logErrors(response);
+      throw errrorMessages;
     case 'Pending-Promotion':
-      console.log("Deployment is in Pending Prmotion Status.");
+      console.log("Deployment is in Pending-Prmotion Status. Respective stakeholder need to promote to next level");
       break;
     default:
       break;
---
+++ custom-actions/pega-trigger-workflow/utils.js
@@ -0,0 +1,18 @@
+function formatJson(json) {
+    return JSON.stringify(json, null, 2);
+  }
+  
+  async function logErrors(response) {
+    let errors = response.data.errors;
+    let errorSummary = "";
+    if (errors.lenth > 0) {
+      await errors.forEach(x => errorSummary += (x.errorText + " and "));
+      return { "message": errorSummary, "full_error": errors };
+    }else{
+      errorTasks = response.data.taskList.filter(x=> x.status !== "Resolved-Completed");
+      if(errorTasks.length > 0){
+        await errorTasks[0].errors.forEach(x => errorSummary += (x.errorMessage + " and "));
+        return { "message": errorSummary};
+      }
+    }
+  }
