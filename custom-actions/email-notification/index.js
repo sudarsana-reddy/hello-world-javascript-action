69f0f135761034c32d525c441a81571a43e7e882
Sud <sudarsana.ala@gmail.com>
2022-08-11 11:45:55 +0530

    using octokit

--- custom-actions/email-notification/index.js
+++ custom-actions/email-notification/index.js
@@ -6,19 +6,17 @@ async function runAction() {
     let token = core.getInput('TOKEN');
     let octokit = github.getOctokit(token);  
     let context = new Context.Context();
-    console.log("ServerURL: ", context.serverUrl);
-    console.log("Owner: ", context.repo.owner);
-    console.log("Repo: ", context.repo.repo);
-    console.log("RunId: ", context.runId);
-    // let workflowRun = await octokit.rest.actions.getWorkflowRun({
-    //     owner: context.repo.owner,
-    //     repo: context.repo.repo,
-    //     run_id: context.runId
-    // });
+   
+    let workflowRun = await octokit.rest.actions.getWorkflowRun({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        run_id: context.runId
+    });
     
-    // console.log("workflow data: ", JSON.stringify(workflowRun.data, null, 2)); 
-    // console.log("workflow run url: ", workflowRun.data.url); 
-    // console.log("workflow run status: ",  workflowRun.status);   
+    console.log("workflow data: ", JSON.stringify(workflowRun.data, null, 2)); 
+    console.log("workflow run html url: ", workflowRun.data.html_url); 
+    console.log("workflow run api url: ",  workflowRun.url);   
+    console.log("workflow run check suite url: ",  workflowRun.data.check_suite_url);   
 }
 
 runAction();
