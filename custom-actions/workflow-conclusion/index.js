eb64d465af19fb031d376d1d2d259fcac86a1e17
Sud <sudarsana.ala@gmail.com>
2022-08-22 10:13:11 +0530

    add check_run_url

--- custom-actions/workflow-conclusion/index.js
+++ custom-actions/workflow-conclusion/index.js
@@ -9,6 +9,9 @@ async function runAction() {
     console.log("repo: ", context.repo.repo)
     console.log("run_id: ", context.runId)
     
+    octokit.rest.checks.get({
+
+    })
     let response = await octokit.rest.actions.listJobsForWorkflowRun({
         owner: context.repo.owner,
         repo: context.repo.repo,
@@ -18,7 +21,7 @@ async function runAction() {
     let jobs = response.data.jobs;
     console.log("Jobs: ", JSON.stringify(jobs, null, 2));
     let jobsWithConclusions= jobs.filter(job => job.conclusion !== null);
-    jobsWithConclusions.forEach(job=> console.log(`Name: ${job.name} - Conclusion: ${job.conclusion} - status: ${job.status}`));
+    jobsWithConclusions.forEach(job=> console.log(`Name: ${job.name} - Conclusion: ${job.conclusion} - status: ${job.status} - check_run_url: ${job.check_run_url}`));    
 }
 
 runAction();
