98be574c7d056006aa181007bfcaa99749c4cea9
Sud <sudarsana.ala@gmail.com>
2022-09-12 17:09:36 +0530

    adding missed dist index file

--- custom-actions/email-notification/dist/index.js
+++ custom-actions/email-notification/dist/index.js
@@ -22413,6 +22413,72 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 2604:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const core = __nccwpck_require__(4824);
+const github = __nccwpck_require__(6985);
+const Context = __nccwpck_require__(9309);
+
+async function getJobData() {
+    let token = core.getInput('TOKEN');
+    let octokit = github.getOctokit(token);
+    let context = new Context.Context();
+    console.log("owner: ", context.repo.owner)
+    console.log("repo: ", context.repo.repo)
+    console.log("run_id: ", context.runId)
+
+    let response = await octokit.rest.actions.listJobsForWorkflowRun({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        run_id: context.runId
+    })
+
+    let jobs = response.data.jobs;
+    console.log("Jobs: ", JSON.stringify(jobs, null, 2));
+    let jobsWithConclusions = jobs.filter(job => job.conclusion !== null);
+
+    let jobStatuses =[];
+
+    for (let index = 0; index < jobsWithConclusions.length; index++) {
+        let job = jobsWithConclusions[index];
+        let jobName = job.name;
+        let jobConclusion = job.conclusion;
+        let jobStatus = job.status;
+        let checkRunUrlSplits = job.check_run_url.split("/");
+        let chekRunId = checkRunUrlSplits[checkRunUrlSplits.length - 1];
+        console.log(chekRunId);
+        let jobAnnotations = await octokit.rest.checks.listAnnotations({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            check_run_id: chekRunId
+        });
+
+        console.log(JSON.stringify(jobAnnotations, null, 2));
+        let annotations = jobAnnotations.data;
+        let annotatiionMessages = "";
+        for (let aIndex = 0; aIndex < annotations.length; aIndex++) {
+            let annotation = annotations[aIndex];
+            console.log(`message: ${annotation.message}`);
+            annotatiionMessages += `${annotation.message}\n`;
+            console.log(`${annotatiionMessages}`);
+        };
+        console.log(`Name: ${jobName} - Conclusion: ${jobConclusion} - Status: ${jobStatus}`);
+        console.log(`${annotatiionMessages}`);
+
+        jobStatuses.push({
+            "name": job.name,
+            "annotations": annotatiionMessages
+        })
+    };
+    console.log(JSON.stringify(jobStatuses, null, 2));
+}
+
+module.exports = { getJobStatus };
+
+
+/***/ }),
+
 /***/ 9323:
 /***/ ((module) => {
 
@@ -22628,6 +22694,8 @@ const Context = __nccwpck_require__(9309);
 const nodemailer = __nccwpck_require__(4526);
 const fs = __nccwpck_require__(7147);
 const path = __nccwpck_require__(1017);
+const jobUtils = __nccwpck_require__(2604);
+
 
 console.log("executing directory: ", __dirname);
 const email_template_file = __nccwpck_require__.ab + "email-template.html";
@@ -22737,7 +22805,7 @@ async function getEmailContent(workflowRunURL) {
 
 async function getFailedJobs() {
     let resultRows = "";
-    let jobData = await getJobData();
+    let jobData = await jobUtils.getJobStatus();
     for (let job of jobData) {
         let trData = `<tr>
                         <td align="center" valign="top">${job.name}</td>
@@ -22750,18 +22818,18 @@ async function getFailedJobs() {
     return resultRows;
 }
 
-async function getJobData() {
-    return [
-        {
-            "name": "Job1",
-            "annotations": "Message1"
-        },
-        {
-            "name": "Job2",
-            "annotations": "Message2"
-        }
-    ]
-}
+// async function getJobData() {
+//     return [
+//         {
+//             "name": "Job1",
+//             "annotations": "Message1"
+//         },
+//         {
+//             "name": "Job2",
+//             "annotations": "Message2"
+//         }
+//     ]
+// }
 
 runAction();
 })();
