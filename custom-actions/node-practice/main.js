81b31f597eb3eaa46d76b9748c611a6b27538693
Sud <sudarsana.ala@gmail.com>
2022-09-20 12:35:32 +0530

    Added token to env

--- custom-actions/node-practice/main.js
+++ custom-actions/node-practice/main.js
@@ -1,5 +1,7 @@
 const DRY_RUN = process.env.DRY_RUN;
 const INPUT_REPOS = process.env.INPUT_REPOS;
+const TOKEN = process.env.TOKEN;
 
 console.log(`DRY_RUN: ${DRY_RUN}`);
 console.log(`INPUT_REPOS: ${INPUT_REPOS}`);
+console.log(`TOKEN: ${TOKEN}`);
