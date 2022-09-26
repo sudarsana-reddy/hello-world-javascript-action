5bf4b9348742bf703da7d4fc8b12589cdaaef1a6
sudarsana-reddy <sudarsana.ala@gmail.com>
2022-09-26 16:52:10 +0530

    Update markdown-utils.ts

--- custom-actions/test-report/src/utils/markdown-utils.ts
+++ custom-actions/test-report/src/utils/markdown-utils.ts
@@ -24,7 +24,7 @@ export function table(headers: ToString[], align: ToString[], ...rows: ToString[
 }
 
 export function tableEscape(content: ToString): string {
-  return content.toString().replace(/'/g, "''").replace('|', '\\|')
+  return content.toString().replace('|', '\\|')
 }
 
 export function fixEol(text?: string): string {
