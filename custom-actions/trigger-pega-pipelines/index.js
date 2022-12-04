65c5510737abfaba354534b85b6280248a3889a7
Sud <sudarsana.ala@gmail.com>
2022-12-04 12:37:12 +0530

    get deployment satus

--- custom-actions/trigger-pega-pipelines/index.js
+++ custom-actions/trigger-pega-pipelines/index.js
@@ -8,24 +8,32 @@ const PEGA_DM_REST_URL = core.getInput('PEGA_DM_REST_URL');
 const PEGA_DM_CLIENT_ID = core.getInput('PEGA_DM_CLIENT_ID');
 const PEGA_DM_CLIENT_SECRET = core.getInput('PEGA_DM_CLIENT_SECRET');
 const JIRA_PEGA_APP_DATA_JSON = core.getInput('JIRA_PEGA_APP_DATA_JSON') || "jira-pega-apps.json";
-const PEGA_PIPELINE_MAPPING_JSON = core.getInput('PEGA_PIPELINE_MAPPING_JSON') || "pega-pipilne-mappings.json";
+const PEGA_PIPELINE_MAPPING_JSON = core.getInput('PEGA_PIPELINE_MAPPING_JSON') || "pega-pipeilne-mappings.json";
+const PEGA_DEPLOYMENT_STATUS_JSON = core.getInput('PEGA_DEPLOYMENT_STATUS_JSON') || "pega-deployment-status.json";
 
 const URL = `${PEGA_DM_REST_URL}/oauth2/v1/token`;
 
 let deploymentFailedPipelines = [];
+let deploymentIds = [];
 async function runAction() {
-   
+
     let hasErrors = false;
     try {
         let appListJson = await getAppListInfo();
         let pipelineMappings = await getPipelinesInfo();
         for (let app of appListJson) {
             try {
+                let pipelineDetails = {
+                    "applicationName": app.applicationName
+                }
                 let pipelineId = pipelineMappings[app.pipelineKey];
                 console.log(`pipelineId:${pipelineId}`);
-
+                pipelineDetails["pieplineId"] = pipelineId;
                 await updatePipeline(pipelineId, app);
-                await triggerPipeline(pipelineId);
+                let data = await triggerPipeline(pipelineId);
+                pipelineDetails["deploymentId"] = data.deploymentId;
+                pipelineDetails["deploymentStatus"] = data.status;
+                deploymentIds.push(pipelineDetails);
             } catch (e) {
                 core.warning(e.message);
                 hasErrors = true;
@@ -35,22 +43,32 @@ async function runAction() {
     } catch (error) {
         console.log(error.message);
         core.setFailed(error.message);
-    }
+    } finally {
+       
+        generateDeolymentStatusJson(deploymentIds);
 
-    if(deploymentFailedPipelines.length > 0){
-        core.warning("Deployment Failed for the following pipelines");
-        core.warning(deploymentFailedPipelines);
-    }
+        if (deploymentFailedPipelines.length > 0) {
+            core.warning("Deployment Failed for the following pipelines");
+            core.warning(deploymentFailedPipelines);
+        }
 
-    if (hasErrors) {
-        let errorMessage = "There are some errors, please take a look"
-        console.log(errorMessage);
-        core.setFailed(errorMessage);
+        if (hasErrors) {
+            let errorMessage = "There are some errors, please take a look"
+            console.log(errorMessage);
+            core.setFailed(errorMessage);
+        }
+
+        
     }
 }
 
+async function generateDeolymentStatusJson(json){
+    console.table(deploymentIds);
+    fs.writeFileSync(PEGA_DEPLOYMENT_STATUS_JSON, JSON.stringify(json, null, 2), 'utf-8');
+}
+
 async function updatePipeline(pipelineId, app) {
-    let pipelineData = await getPipelineData(pipelineId);    
+    let pipelineData = await getPipelineData(pipelineId);
     if (pipelineData.applicationName !== app.applicationName || pipelineData.applicationVersion !== app.applicationVersion) {
         let errorMessage = "The Targeted App Name and Version are not matching with Pipelinedata. Verify the pipleine mapping.";
         console.log(errorMessage);
@@ -87,7 +105,7 @@ async function updatePipeline(pipelineId, app) {
     let response = await fetch(url, config);
     let data = await response.json();
     console.log(JSON.stringify(data, null, 2));
-    if(data.errors && data.errors.length > 0){
+    if (data.errors && data.errors.length > 0) {
         deploymentFailedPipelines.push(data.pipelineName);
         let errorMessage = `${data.applicationName} - Update Pipleine Failed\n`;
 
@@ -105,19 +123,21 @@ async function triggerPipeline(pipelineId) {
         method: 'POST',
         headers: {
             'Authorization': `Bearer ${access_token}`
-        }        
+        }
     };
 
     let response = await fetch(url, config);
     let data = await response.json();
     console.log(JSON.stringify(data, null, 2));
-    if(data.errors && data.errors.length > 0){
+    if (data.errors && data.errors.length > 0) {
         deploymentFailedPipelines.push(data.pipelineName);
         let errorMessage = `${data.applicationName} - Trigger Pipleine Failed\n`;
 
         data.errors.forEach(error => errorMessage = errorMessage + `${error.errorText}\n`);
         throw new Error(errorMessage);
     }
+
+    return data;
 }
 
 async function getPipelineData(pipelineId) {
